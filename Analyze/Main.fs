bfdf837619cf5c7fd38d7966ae01aff7fa5d096c
Florian Biermann <fbie@itu.dk>
2015-03-19 16:02:15 +0100

    drop try-with construct

--- Analyze/Main.fs
+++ Analyze/Main.fs
@@ -12,39 +12,36 @@ let analyze (file: string) =
   let trunc f =
     Seq.choose (fun x -> let t = f x in if t >= start || t <= duration then Some x else None)
 
-  try
-    let progress = entries
-                   |> Seq.toList
-                   |> Session.progress2
-                   |> Seq.map (fun x -> fst x, snd x / 17.0)
-    let pupils = raw
-                 |> trunc (fun x -> x.aT - x.startT)
-                 |> Session.pupilSize
+  let progress = entries
+                 |> Seq.toList
+                 |> Session.progress2
+                 |> Seq.map (fun x -> fst x, snd x / 17.0)
+  let pupils = raw
+               |> trunc (fun x -> x.aT - x.startT)
+               |> Session.pupilSize
 
-    Chart.subplot 3 1
-    Chart.xlim (float start) (float duration)
-    Chart.ylim -0.5 0.5
-    Chart.yaxis "Dilation (normalized)"
-    Chart.lines "pupils" pupils "alpha=0.5"
+  Chart.subplot 3 1
+  Chart.xlim (float start) (float duration)
+  Chart.ylim -0.5 0.5
+  Chart.yaxis "Dilation (normalized)"
+  Chart.lines "pupils" pupils "alpha=0.5"
 
-    Chart.subplot 3 1
-    Chart.xlim (float start) (float duration)
-    Chart.ylim 0.0 1.1
-    Chart.yaxis "Progress (normalized)"
-    Chart.lines "progress" progress "width=2"
+  Chart.subplot 3 1
+  Chart.xlim (float start) (float duration)
+  Chart.ylim 0.0 1.1
+  Chart.yaxis "Progress (normalized)"
+  Chart.lines "progress" progress "width=2"
 
-    Chart.subplot 3 1
-    Chart.xlim (float start) (float duration)
-    Chart.ylim 0.0 1.0
-    Chart.xaxis "Time (s)"
-    Chart.points "attention" ((Seq.toList >> Session.attention >> (Session.toPoints 0.5) >> trunc fst) entries) ""
-    Chart.points "zoom" ((Session.zoom >> (Session.toPoints 0.3) >> trunc fst) entries) ""
-    Chart.points "rotate" ((Session.rotate >> (Session.toPoints 0.1) >> trunc fst) entries) ""
+  Chart.subplot 3 1
+  Chart.xlim (float start) (float duration)
+  Chart.ylim 0.0 1.0
+  Chart.xaxis "Time (s)"
+  Chart.points "attention" ((Seq.toList >> Session.attention >> (Session.toPoints 0.5) >> trunc fst) entries) ""
+  Chart.points "zoom" ((Session.zoom >> (Session.toPoints 0.3) >> trunc fst) entries) ""
+  Chart.points "rotate" ((Session.rotate >> (Session.toPoints 0.1) >> trunc fst) entries) ""
 
-    Chart.legend ""
-    Chart.plotdone ""
-  with
-    | _ -> printfn ""
+  Chart.legend ""
+  Chart.plotdone ""
 
 [<EntryPoint>]
 let main argv =
