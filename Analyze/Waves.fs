417de8f5450c10a10c4c4eb906f84c20751014e6
Florian Biermann <fbie@itu.dk>
2015-03-19 16:01:18 +0100

    make sure not to call transform on arrays that are too short for a given h

--- Analyze/Waves.fs
+++ Analyze/Waves.fs
@@ -7,23 +7,30 @@ let private window i n (arr: 'a []) =
   [| for j in 0 .. n - 1 do yield arr.[i + j] |]
 
 let private wrap n (arr: 'a []) =
-  let l = Array.length arr - 1
-  [| for j in -(n >>> 1) .. (n >>> 1) - 1 do yield if j < 0 then arr.[l + j] else arr.[j] |]
+  let l = Array.length arr
+  [| for j in -(n >>> 1) .. (n >>> 1) - 1 do
+        if j < 0 then
+            yield arr.[l + j]
+        else
+            yield arr.[j] |]
 
 // See http://fsharpnews.blogspot.dk/2006/12/array-manipulation-continued.html
 let rec private transform h g a n =
-  let n2 = n >>> 1 in
-  let d = Array.length h
-  let tmp = Array.create n 0.
-  for i = 0 to (n2 - d / 2) - 1 do
-    let win = window (i * 2) d a
-    tmp.[i] <- dot win h
-    tmp.[i + n2] <- dot win g
-  let wrap = wrap d a
-  tmp.[n2 - 1] <- dot wrap h
-  tmp.[2 * n2 - 1] <- dot wrap g
-  Array.blit tmp 0 a 0 n;
-  if n > d then transform h g a n2 else a
+  let d = h |> Array.length
+  if n > d then
+    let n2 = n >>> 1 in
+    let tmp = Array.create n 0.
+    for i = 0 to (n2 - d / 2) - 1 do
+      let win = window (i * 2) d a
+      tmp.[i] <- dot win h
+      tmp.[i + n2] <- dot win g
+    let wrap = wrap d a
+    tmp.[n2 - 1] <- dot wrap h
+    tmp.[2 * n2 - 1] <- dot wrap g
+    Array.blit tmp 0 a 0 n;
+    transform h g a n2
+  else
+    a
 
 let d2 a =
   let h = [| -0.12940952255092145; 0.22414386804185735; 0.836516303737469; 0.48296291314469025 |]
