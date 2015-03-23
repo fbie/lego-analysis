e6f4a64418930930d3234c6de21489124121d020
Florian Biermann <fbie@itu.dk>
2015-03-23 17:05:40 +0100

    annotate y-axis for bar charts

--- Analyze/Main.fs
+++ Analyze/Main.fs
@@ -33,6 +33,7 @@ let analyze (file: string) =
 
   Chart.subplot 3 1
   Chart.xlim (float start) (float duration)
+  Chart.yaxis "Time spent (s)"
   Chart.xaxis "Time (s)"
   Chart.bars "attention" (entries |> Session.attention)
 //  Chart.points "zoom" ((Session.zoom >> (Session.toPoints 0.3) >> trunc fst) entries) ""
