465ba5e52a27ddd113508c0a76a79343efa69bd5
Florian Biermann <fbie@itu.dk>
2015-03-30 10:48:17 +0200

    do not try to mkdir if you'll never use it

--- aggregate.sh
+++ aggregate.sh
@@ -1,5 +1,3 @@
 BASEDIR=$(cd $(dirname $0); pwd)
-PLOTS=$BASEDIR/plots
-mkdir -p $PLOTS
 FILES=$(for fn in $@; do echo $(cd $(dirname $1); echo "$(pwd)/$(basename $1)"); done)
 mono -O=all $BASEDIR/Analyze/bin/Release/Analyze.exe $FILES | python $BASEDIR/plot.py
