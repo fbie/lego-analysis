0b0c322bd5ca9b8fb9862025a34032501fec57cd
Florian Biermann <fbie@itu.dk>
2015-03-31 16:09:20 +0200

    add errbars function

--- Analyze/Chart.fs
+++ Analyze/Chart.fs
@@ -6,12 +6,13 @@ open Analyze.Time
 let private untime (a: (float<_> * float<_>) seq) =
   a |> Seq.map (fun (x, y) -> float x, float y)
 
+let private ps =
+  Seq.map (fun (x, y) ->
+           "(" + sprintf "%f" x + "," + sprintf "%f" y + ")")
+  >> Seq.toList
+  >> String.concat ","
+
 let private chart kind title data =
-  let ps =
-    Seq.map (fun (x, y) ->
-             "(" + sprintf "%f" x + "," + sprintf "%f" y + ")")
-    >> Seq.toList
-    >> String.concat ","
   printfn "cmd=%s;label=%s;data=%s;%s" kind title (data |> untime |> ps)
 
 let points title data =
@@ -22,6 +23,12 @@ let lines title data =
 
 let bars title data width =
   chart "bars" title data (sprintf "width=%f" width)
+
+let errbars title data width =
+  let xvals = Seq.map fst data
+  let yvals = Seq.map snd data |> Seq.map fst
+  let yerr  = Seq.map snd data |> Seq.map snd
+  chart "bars" title (Seq.zip xvals yvals) (sprintf "width=%f;yerr=%s" width ((Seq.zip xvals yerr) |> untime |> ps))
 
 let xaxis =
   printfn "cmd=xaxis;label=%s"
