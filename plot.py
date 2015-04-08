e830360b2949af6ac2f597e5a8a177980a4dffcb
Florian Biermann <fbie@itu.dk>
2015-04-08 11:28:48 +0200

    use defaultdict to provide consistent colors for each step

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from collections import defaultdict
 import string
 import sys
 import matplotlib.pyplot as plt
@@ -16,15 +17,15 @@ def parse():
                 continue
 
 def colors(n):
-    print n
     cm = plt.get_cmap('gist_rainbow')
     for i in xrange(n):
         yield cm(1.0 * i / n)
 
 def mkColorMap(n):
-    return dict(map(lambda (i, c): (i + 1, c), enumerate(colors(n))))
+    c = colors(n)
+    return defaultdict(lambda: c.next())
 
-def mkAbsolute(s, x):
+def undiff(s, x):
     for i in xrange(len(x)):
         t = x[i]
         x[i] = s
@@ -34,8 +35,8 @@ def mkAbsolute(s, x):
 if __name__ == '__main__':
     vals = list(parse())
     cm = mkColorMap(27)
-    cl = map(lambda x: cm[int(x[0])], vals)
-    x_vals = mkAbsolute(0.0, [v[1] for v in vals])
+    cl = map(lambda x: cm[x[0]], vals)
+    x_vals = undiff(0.0, [v[1] for v in vals])
     y_vals = [v[2] / v[1] for v in vals]
     w_vals = [v[1] for v in vals]
     plt.bar(left=x_vals,
@@ -44,4 +45,6 @@ if __name__ == '__main__':
             linewidth=0.0,
             color=cl)
     plt.xlabel('Time (s)')
+    plt.xlim((0.0, x_vals[-1]))
+    plt.ylabel('Attention (s) / Duration (s)')
     plt.show()
