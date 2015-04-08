e8e912674f993b5d0826379882bbe5801eb22bf5
Florian Biermann <fbie@itu.dk>
2015-04-08 10:08:53 +0200

    change plot.py to show new csv data format

--- plot.py
+++ plot.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-from collections import defaultdict
 import string
 import sys
 import matplotlib.pyplot as plt
@@ -9,113 +8,44 @@ COLORS = ('b', 'g', 'r', 'c', 'm', 'y', 'k')
 LINES = ('-', '--', '-.', ':')
 MARKERS = ('.', ',', 'o', 'v', '^', '<', '>', 's', '*', '+')
 
-DEFAULT = { 'label': None, 'alpha': 1.0, 'width': 1, 'height': 1, 'yerr': None }
+def parse():
+    for l in sys.stdin:
+        if not (l in string.whitespace or l.startswith('#')):
+            try:
+                vals = map(float, l.split(';'))
+                if not vals[0] == 0.0:
+                    yield vals
+            except Exception as e:
+                print e
+                continue
 
-class circularlist:
-    def __init__(self, l):
-        self._l = l
-        self._i = 0
+def colors(n):
+    print n
+    cm = plt.get_cmap('gist_rainbow')
+    for i in xrange(n):
+        yield cm(1.0 * i / n)
 
-    def next(self):
-        v = self._l[self._i]
-        self._i = (self._i + 1) % len(self._l)
-        return v
+def mkColorMap(n):
+    return dict(map(lambda (i, c): (i + 1, c), enumerate(colors(n))))
 
-class superdefaultdict(defaultdict):
-    def __init__(self, vals):
-        defaultdict.__init__(self, None, vals)
+def mkAbsolute(s, x):
+    for i in xrange(len(x)):
+        t = x[i]
+        x[i] = s
+        s += t
+    return x
 
-    def __getitem__(self, key):
-        val = self.get(key)
-        if not val:
-            return DEFAULT[key]
-        return val
-
-class Colors:
-    def __init__(self):
-        self.line = circularlist(list(c + l for l in LINES for c in COLORS))
-        self.marker = circularlist(list(c + m for m in MARKERS for c in COLORS))
-        self.colors = circularlist(COLORS)
-
-class Plotter:
-    def __init__(self):
-        self.colors = Colors()
-        self.sp_last = None
-        self.sp_id = 1
-        self.sp_bar_offset = 0.0
-        plt.figure(figsize=(16, 9), dpi=92)
-
-    def points(self, cmd):
-        data = eval(cmd['data'])
-        s = self.colors.marker.next()
-        plt.scatter([x for x,y in data],
-                    [y for x,y in data],
-                    c=s[0], marker=s[1],
-                    alpha=0.5,
-                    label=cmd['label'],
-                    antialiased=True)
-
-    def lines(self, cmd):
-        data = eval(cmd['data'])
-        plt.plot([x for x,y in data],
-                 [y for x,y in data],
-                 self.colors.line.next(),
-                 label=cmd['label'],
-                 alpha=float(cmd['alpha']),
-                 linewidth=int(cmd['width']),
-                 antialiased=True)
-
-    def bars(self, cmd):
-        data = eval(cmd['data'])
-        plt.bar(left=[x + self.sp_bar_offset for x,y in data],
-                height=[y for x,y in data],
-                width=float(cmd['width']),
-                label=cmd['label'],
-                color=self.colors.colors.next(),
-                yerr=cmd['yerr'],
-                linewidth=0)
-        self.sp_bar_offset += float(cmd['width'])
-
-    def xaxis(self, cmd):
-        plt.xlabel(cmd['label'])
-
-    def xlim(self, cmd):
-        plt.xlim((float(cmd['xmin']), float(cmd['xmax'])))
-
-    def yaxis(self, cmd):
-        plt.ylabel(cmd['label'])
-
-    def ylim(self, cmd):
-        plt.ylim((float(cmd['ymin']), float(cmd['ymax'])))
-
-    def subplot(self, cmd):
-        self.sp_last = plt.subplot(int(cmd['width']), int(cmd['height']), self.sp_id)
-        self.sp_id += 1
-        self.sp_bar_offset = 0.0
-        self.colors = Colors()
-
-    def legend(self, cmd):
-        if self.sp_last:
-            self.sp_last.legend()
-        else:
-            plt.legend()
-
-    def done(self, cmd):
-        args = sys.argv[1:]
-        if len(args) != 0:
-            plt.savefig(args[0].split('.')[0] + '.png')
-        else:
-            plt.show()
-        sys.exit(0)
-
-plotter = Plotter()
-for l in sys.stdin:
-    if l in string.whitespace:
-        continue
-    line = map(lambda x: x.split('='), [s for s in l.strip().split(';') if s])
-    try:
-        cmd = superdefaultdict(line)
-        eval('plotter.' + cmd['cmd'])(cmd)
-    except Exception as e:
-        print("Error: %s\nInput:%s", (e, l))
-sys.exit(1)
+if __name__ == '__main__':
+    vals = list(parse())
+    cm = mkColorMap(27)
+    cl = map(lambda x: cm[int(x[0])], vals)
+    x_vals = mkAbsolute(0.0, [v[1] for v in vals])
+    y_vals = [v[2] / v[1] for v in vals]
+    w_vals = [v[1] for v in vals]
+    plt.bar(left=x_vals,
+            height=y_vals,
+            width=w_vals,
+            linewidth=0.0,
+            color=cl)
+    plt.xlabel('Time (s)')
+    plt.show()
