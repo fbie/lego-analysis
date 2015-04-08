74a9597600541d370b829c555df766d2928509a9
Florian Biermann <fbie@itu.dk>
2015-04-08 16:53:05 +0200

    call python from within script

--- aggregate.sh
+++ aggregate.sh
@@ -1,3 +1,3 @@
 BASEDIR=$(cd $(dirname $0); pwd)
 FILES=$(for fn in $@; do echo $(cd $(dirname $fn); echo "$(pwd)/$(basename $fn)"); done)
-mono -O=all $BASEDIR/Analyze/bin/Release/Analyze.exe $FILES #| python $BASEDIR/plot.py
+mono -O=all $BASEDIR/Analyze/bin/Release/Analyze.exe $FILES | python $BASEDIR/plot.py
