d878935848aff1a464e2ddff52b67d6cf8196eae
Florian Biermann <fbie@itu.dk>
2015-04-08 15:44:14 +0200

    fix broken aggregate script

--- aggregate.sh
+++ aggregate.sh
@@ -1,3 +1,3 @@
 BASEDIR=$(cd $(dirname $0); pwd)
-FILES=$(for fn in $@; do echo $(cd $(dirname $1); echo "$(pwd)/$(basename $1)"); done)
-mono -O=all $BASEDIR/Analyze/bin/Release/Analyze.exe $FILES | python $BASEDIR/plot.py
+FILES=$(for fn in $@; do echo $(cd $(dirname $fn); echo "$(pwd)/$(basename $fn)"); done)
+mono -O=all $BASEDIR/Analyze/bin/Release/Analyze.exe $FILES #| python $BASEDIR/plot.py
