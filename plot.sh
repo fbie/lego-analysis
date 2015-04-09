7fcc95f346b6ae1a18449613c7f6168b876a76ef
Florian Biermann <fbie@itu.dk>
2015-04-09 15:52:42 +0200

    remove unused scripts

--- plot.sh
+++
@@ -1,4 +0,0 @@
-BASEDIR=$(cd $(dirname $0); pwd)
-PLOTS=$BASEDIR/plots
-mkdir -p $PLOTS
-mono -O=all $BASEDIR/Analyze/bin/Release/Analyze.exe $(cd $(dirname $1); echo "$(pwd)/$(basename $1)") | python $BASEDIR/plot.py $PLOTS/$(basename $1)
